2b33526339281c061d234592285e7d02c1a9ff84
wueghbfc <1547436991@qq.com>
2025-11-22 20:51:35 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {}
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    define: {
+      // Define process.env globally so accessing process.env.API_KEY works in the app
+      // We bake the build-time environment variable value into the code
+      'process.env': {
+        API_KEY: env.API_KEY
+      }
+    }
   }
 })
